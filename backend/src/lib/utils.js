c28e498a57665dfcd4adf4fe4e65db0ad95fa9c5
Harsh Hirawat <106510913+GreenHacker420@users.noreply.github.com>
2025-01-31 13:21:47 +0530

    bankend done

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,18 +1,16 @@
-import { max } from "@splidejs/splide/src/js/utils";
 import jwt from "jsonwebtoken";
+
 export const generateToken = (userId, res) => {
-    
-    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
-        expiresIn: "7d",
-    });
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
 
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
+    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+    secure: process.env.NODE_ENV !== "development",
+  });
 
-    res.cookie("jwt", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== "development",
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        sameSite : "strict",
-    });
-
-    return token;
-}
+  return token;
+};
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,15 @@
-import express from 'express';
-import {signup} from '../controllers/auth.controller.js';
-import {login} from '../controllers/auth.controller.js';
-import {logout} from '../controllers/auth.controller.js';
-
+import express from "express";
+import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-
 router.post("/signup", signup);
-
 router.post("/login", login);
-
 router.post("/logout", logout);
 
+router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
